8e675f38d3517f0469a876e0b48a952e2a24fd39
jmechristian <jmechristian@gmail.com>
2024-07-23 09:53:47 -0400

    ssss

--- src/components/HeroGroup.js
+++ src/components/HeroGroup.js
@@ -5,7 +5,7 @@ import { useRive, useStateMachineInput } from '@rive-app/react-canvas';
 const HeroGroup = () => {
   const { rive, RiveComponent } = useRive({
     // Load a local riv `clean_the_car.riv` or upload your own!
-    src: 'https://packschool.s3.amazonaws.com/sa-intro-group.riv',
+    src: 'https://packschool.s3.amazonaws.com/sa-intro-group-2.riv',
     stateMachines: 'State Machine 1',
     // Be sure to specify the correct state machine (or animation) name
     onLoadError: (err) => console.log(err),
